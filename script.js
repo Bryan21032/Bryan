ff93b9bbf07869411d9ad5418c7d3c75226c4505
Bryan21032 <115647445+Bryan21032@users.noreply.github.com>
2023-03-30 12:02:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 ;
+function isCompatible(item) {
+  // En la vida real es muy probable que tenga aquí reglas más complejas
+  return ['Chromium', 'Google Chrome', 'NewBrowser'].includes(item.brand);
+}
+navigator.userAgentData;
+
 if ('serviceWorker' in navigator.userAgentData) {
     navigator.userAgentData.serviceWorker.register('./sw.js')
       .then(reg => console.log('Registro de SW exitoso', reg))
